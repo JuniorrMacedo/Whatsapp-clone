8b2952d4ce1c4660d83b218e6da1bc4e80fa65f4
Idelmario Macedo <idelmariocorreia@hotmail.com>
2023-01-18 13:19:24 -0300

    feat: create chat component
     On branch master
     Changes to be committed:
    	modified:   src/components/ChatDetail.jsx
    
    footer: this commit is related to the task #2

--- src/components/ChatDetail.jsx
+++ src/components/ChatDetail.jsx
@@ -1,34 +1,89 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import Message from "./Message";
 import RoundedBtn from "./Common/RoundedBtn";
+import { LoadMessagesController } from "../Controllers/LoadMessagesController"; 
 import { MdSearch, MdSend } from "react-icons/md";
 import { HiDotsVertical } from "react-icons/hi";
 import { BiHappy } from "react-icons/bi";
 import { FiPaperclip } from "react-icons/fi";
 import { IoMdMic } from "react-icons/io";
-import { cs1 } from "../assets/whatsapp"
+import { chat5 } from "../assets/whatsapp"
+import { getTime } from "../logic/whatsapp";
+
+const messagesData = new LoadMessagesController();
 
 function ChatDetail() {
+  const [messages, setMessages] = useState(messagesData);
+  const [typing, setTyping] = useState(false);
+
+  const inputRef = useRef(null);
+  const bottomRef = useRef(null);
+
+  // Funções
+
+  // adicionar mensagem
+  const addMessage = (msg) => {
+    const newMessages = [...messages, msg]
+    setMessages(newMessages);
+  };
+
+  // mostrar botao de mic/enviar
+  const handleInputChange = () => {
+    inputRef.current.value.length === 0 ? setTyping(false) : setTyping(true);
+  };
+
+  // enviar mensagem
+  const handleInputSubmit = () => {
+    if (inputRef.current.value.length > 0) {
+      addMessage({
+        msg: inputRef.current.value,
+        time: getTime(),
+        sent: true,
+      });
+      inputRef.current.value = "";
+      inputRef.current.focus();
+      setTyping(false);
+    }
+  };
+
+  // Descer tela para mensagem enviada
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [messages]);
+
+  // Enviar com enter
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.code === "Enter") handleInputSubmit(); 
+    };
+    
+    document.addEventListener("keydown", listener);
+    return () => document.removeEventListener("keydown", listener);
+  });
+
   return (
     // Conversa container master
     <div className="flex flex-col h-screen">
       {/* Área do contato */}
       <div className="flex justify-between bg-[#202d33] h-[60px] p-3">
-        {/* Contact info */}
+        {/* Info contato */}
         <div className="flex items-center">
-          {/* Profile picture */}
-          <img src={cs1} alt="profile_picture" className="rounded-full w-[40px] h-[40px] mr-5"/>
+          {/* Foto de perfil */}
+          <img src={chat5} alt="profile_picture" className="rounded-full w-[40px] h-[40px] mr-5"/>
 
           {/* Info */}
           <div className="flex flex-col">
-            {/* Contact */}
-            <h1 className="text-white font-medium ">Dev</h1>
+            {/* Contato */}
+            <h1 className="text-white font-medium ">Maria</h1>
 
             {/* Status */}
             <p className="text-[#8796a1] text-xs">Online</p>
           </div>
         </div>
 
-        {/* Buttons */}
+        {/* Botões */}
         <div className="flex justify-between items-center w-[85px]">
           <RoundedBtn icon={<MdSearch/>}/>
           <RoundedBtn icon={<HiDotsVertical/>}/>
@@ -39,13 +94,22 @@ function ChatDetail() {
       <div className="bg-[#0a131a] bg-[url('assets/images/bg.webp')] bg-contain overflow-y-scroll h-100"
       style={{ padding: "12px 7%" }}
       >
-        
+        {messages.map((msg) => (
+          <Message 
+            msg={msg.msg}
+            time={msg.time}
+            isLink={msg.isLink}
+            img={msg.img}
+            sent={msg.sent}
+          />
+        ))}
+        <div ref={bottomRef}/>
       </div>
 
       {/* seção inferior */}
       <div className="flex items-center bg-[#202d33] w-100 h-[70px] p-2">
         {/* Botão emoji */}
-        <RoundedBtn icon={<BiHappy />}/>
+        <RoundedBtn icon={<BiHappy />} />
 
         {/* Botão upload */}
         <span className="mr-2">
@@ -57,12 +121,17 @@ function ChatDetail() {
           type="text"
           placeholder="Mensagem"
           className="bg-[#2c3943] rounded-lg outline-none text-sm text-neutral-200 w-100 h-100 px-3 placeholder:text-sm placeholder:text-[#8796a1]"
+          onChange={handleInputChange}
+          ref={inputRef}
         />
 
-        {/* Botão Mic/Send */}
+        {/* Botão Mic/Enviar */}
         <span className="ml-2">
-          <RoundedBtn icon={<IoMdMic />}/>
-          {/* <RoundedBtn icon={<MdSend />}/> */}
+          {typing ? (
+            <RoundedBtn icon={<MdSend /> } onClick={handleInputSubmit} />
+          ) : (
+            <RoundedBtn icon={<IoMdMic />} />
+          )}         
         </span>
       </div>
     </div>
