4f3499a84914df8423fa80dcdf7f9c9f9f782288
Idelmario Macedo <idelmariocorreia@hotmail.com>
2023-01-19 15:51:46 -0300

    feat: create function to load contacts
     On branch master
     Changes to be committed:
    	modified:   src/Controllers/LoadContacts.controller.js
    
    footer: this commit is related to the task #8

--- src/Controllers/LoadContacts.controller.js
+++ src/Controllers/LoadContacts.controller.js
@@ -1,35 +1,35 @@
-import {chat1, chat2, chat3} from "../assets/whatsapp";
+import {chat5, chat2, chat3} from "../assets/whatsapp";
 
 
 export function LoadContacts(){
   
   const responseBackend = {
     "contacts": [
-    {
-      "pp": chat1,
-      "contact": "Maria",
-      "telephone": "+55 75 9988-5544",
-      "msg": "Ficou animal, obrigado!",
-      "time": "13:15",
-      "unreadMsgs": null,
-    },
-    {
-      "pp": chat2,
-      "contact": "Pedro",
-      "telephone": "+55 75 9988-5544",
-      "msg": "Ficou animal, obrigado!",
-      "time": "13:15",
-      "unreadMsgs": 2,
-    }, {
-      "pp": chat3,
-      "contact": "João",
-      "telephone": "+55 75 9988-5544",
-      "msg": "Ficou animal, obrigado!",
-      "time": "13:15",
-      "unreadMsgs": null,
-    }
-    
-  ]};
+      {
+        "pp": chat5,
+        "contact": "Maria",
+        "telephone": "+55 75 9988-5544",
+        "msg": "Ficou animal, obrigado!",
+        "time": "13:15",
+        "unreadMsgs": null,
+      },
+      {
+        "pp": chat2,
+        "contact": "Pedro",
+        "telephone": "+55 75 9988-5544",
+        "msg": "Fala, irmão!!",
+        "time": "13:07",
+        "unreadMsgs": 2,
+      }, {
+        "pp": chat3,
+        "contact": "João",
+        "telephone": "+55 75 9988-5544",
+        "msg": "Preciso falar com você, me liga!",
+        "time": "13:00",
+        "unreadMsgs": null,
+      } 
+    ]
+  };
 
   const contacts = responseBackend.contacts;
   
